963da76a0c000a3eac9a6644275fc6a7f4d7eec6
Sergey Starukhin <star.s@me.com>
2024-02-22 12:41:45 +0100

    simplify AppKit error presentation

--- Sources/ErrorPresentation/ErrorPresentation+AppKit.swift
+++ Sources/ErrorPresentation/ErrorPresentation+AppKit.swift
@@ -3,40 +3,25 @@ import AppKit
 
 @objc
 extension NSResponder {
-    
     open func presentError(_ error: Error, didPresentHandler handler: @escaping (_ recovered: Bool) -> Void) {
         (nextResponder ?? NSApplication.shared).presentError(willPresentError(error), didPresentHandler: handler)
     }
 }
 
 @objc
-public protocol ErrorPresentationApplicationDelegate: NSApplicationDelegate {
-	@objc
-	optional func application(_ application: NSApplication, shouldPassErrorToNextResponder error: Error) -> Bool
-	@objc
-	optional func application(_ application: NSApplication, shouldSkipErrorPresentation error: Error) -> Bool
-}
-
-@objc
 extension NSApplication {
-
-	private typealias PresentationAnchor = NSWindow
-
-    override open func presentError(_ error: Error, didPresentHandler handler: @escaping (_ recovered: Bool) -> Void) {
+    open override func presentError(_ error: Error, didPresentHandler handler: @escaping (_ recovered: Bool) -> Void) {
         let error = willPresentError(error)
-		if let next = nextResponder, shouldPassErrorToNextResponder(error) {
-			next.presentError(error, didPresentHandler: handler)
-			return
-		}
-		if shouldSkipPresentingError(error) {
+
+        if error.isCancelled {
 			handler(false)
 			return
 		}
-        guard let anchor = presentationAnchor(for: error) else {
+        guard let window = windows.first(where: { $0.isKeyWindow && $0.isVisible }) else {
 			handler(false)
             return
         }
-		NSAlert(error: error).beginSheetModal(for: anchor) {
+		NSAlert(error: error).beginSheetModal(for: window) {
 			guard let error = error as? RecoverableError else {
 				handler(false)
 				return
@@ -44,29 +29,10 @@ extension NSApplication {
 			error.attemptRecovery(optionIndex: $0.recoveryOptionIndex, resultHandler: handler)
 		}
     }
-
-	private func presentationAnchor(for error: Error) -> PresentationAnchor? {
-		windows.first(where: { $0.isKeyWindow && $0.isVisible })
-	}
-
-	private func shouldPassErrorToNextResponder(_ error: Error) -> Bool {
-		guard let delegate = delegate as? ErrorPresentationApplicationDelegate else {
-			return false
-		}
-		return delegate.application?(self, shouldPassErrorToNextResponder: error) ?? false
-	}
-
-	private func shouldSkipPresentingError(_ error: Error) -> Bool {
-		guard let delegate = delegate as? ErrorPresentationApplicationDelegate else {
-			return error.isCancelled
-		}
-		return delegate.application?(self, shouldSkipErrorPresentation: error) ?? error.isCancelled
-	}
 }
 
 @objc
 extension NSWindowController {
-    
     override open func presentError(_ error: Error, didPresentHandler handler: @escaping (_ recovered: Bool) -> Void) {
         guard let document = document as? NSDocument else {
             super.presentError(error, didPresentHandler: handler)
@@ -78,7 +44,6 @@ extension NSWindowController {
 
 @objc
 extension NSDocumentController {
-    
     open func presentError(_ error: Error, didPresentHandler handler: @escaping (_ recovered: Bool) -> Void) {
         NSApplication.shared.presentError(willPresentError(error), didPresentHandler: handler)
     }
@@ -86,7 +51,6 @@ extension NSDocumentController {
 
 @objc
 extension NSDocument {
-    
     open func presentError(_ error: Error, didPresentHandler handler: @escaping (_ recovered: Bool) -> Void) {
         NSDocumentController.shared.presentError(willPresentError(error), didPresentHandler: handler)
     }

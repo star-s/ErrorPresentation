782d9a91a9f6c23f64829fddfa5ccafe2798c778
Sergey Starukhin <star.s@me.com>
2024-02-22 18:07:28 +0100

    add ErrorPresentationSceneDelegate protocol

--- Sources/ErrorPresentation/ErrorPresentation+UIKit.swift
+++ Sources/ErrorPresentation/ErrorPresentation+UIKit.swift
@@ -13,6 +13,29 @@ extension UIResponder {
     }
 }
 
+// MARK: - UIScene
+
+@available(iOS 13.0, tvOS 13.0, macOS 10.15, watchOS 6.0, *)
+@objc
+public protocol ErrorPresentationSceneDelegate: UISceneDelegate {
+    @objc
+    func scene(_ scene: UIScene, willPresentError error: Error) -> Error
+}
+
+@available(iOS 13.0, tvOS 13.0, macOS 10.15, watchOS 6.0, *)
+@objc
+extension UIScene {
+
+    open override func willPresentError(_ error: Error) -> Error {
+        guard let delegate = delegate as? ErrorPresentationSceneDelegate else {
+            return super.willPresentError(error)
+        }
+        return delegate.scene(self, willPresentError: error)
+    }
+}
+
+// MARK: - UIApplication
+
 @objc
 public protocol ErrorPresentationApplicationDelegate: UIApplicationDelegate {
     @objc
